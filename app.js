4d946ee7bae5986eb8288eea295bc40438ff6862
WangX92 <need91dev@gmail.com>
2021-07-26 19:47:00 +0800

    auth/user system

--- app.js
+++ app.js
@@ -1,21 +1,63 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-//database
-const db = require('./config/database')
-
-db.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log("Error: " + err))
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-app.get('/', (req, res) => res.send('INDEX'));
 
-//Git routes
-app.use('/gigs', require('./routes/gigs'));
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//database
+// const db = require('./app/config/database')
+
+// db.authenticate()
+//     .then(() => console.log('Database connected...'))
+//     .catch(err => console.log("Error: " + err))
+
+const db = require("./app/models");
+const Role = db.role;
+
+// db.sequelize.sync();
+// force: true will drop the table if it already exists
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Database with { force: true }');
+    initial();
+});
+
+//Routes
+app.get('/', (req, res) => res.send('Welcome to my application.'));
+// app.use('/gigs', require('./app/routes/gigs'));
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+
+// https://www.youtube.com/watch?v=bOHysWYMZM0
